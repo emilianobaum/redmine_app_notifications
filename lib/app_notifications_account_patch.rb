76a01f6b4fb3ef36c3bfa0f939b6b683ef718c0f
Emiliano Baum <ebaum@conae.gov.ar>
2026-02-22 04:11:42 +0000

    Adaptar plugin para Rails 7 y solucionar error de tabla inexistente
    - Eliminar llamada a unloadable (no existe en Rails 7)
    - Agregar verificación de existencia de tabla en init.rb
    - Proteger asociaciones del modelo AppNotification
    - Agregar inicializador para excluir tabla del schema dump si no existe
    - Usar data_source_exists? en lugar de table_exists?

--- lib/app_notifications_account_patch.rb
+++ lib/app_notifications_account_patch.rb
@@ -5,8 +5,6 @@ module AppNotificationsAccountPatch
     base.send(:include, InstanceMethods)
 
     base.class_eval do
-      unloadable # Send unloadable so it will not be unloaded in development
-
       alias_method :account_without_in_app_option, :account
       alias_method :account, :account_with_in_app_option
     end
